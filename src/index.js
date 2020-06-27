007d7197fad44f8715dd31ba795c03687a2728d9
Jim Johnston <hello@breadnbutterdesign.com>
2020-06-27 23:24:01 +0100

    nonvector test

--- src/index.js
+++ src/index.js
@@ -8,54 +8,48 @@ const sketch = (p) => {
   let x = 100;
   let y = 100;
   let counter = 0;
-  let curpos, prevpos, radius;
+  let curpos, plotpos, plotrad, radius, dist;
+  plotpos = [0,0]
+  plotrad = 0
   
   
 
   p.setup = function() {
     p.createCanvas(res[0], res[1]);
-    p.frameRate(10)
+    //p.frameRate(10)
     console.log(p)
   };
 
   p.draw = function() {
-    let fps = p.frameRate();
-    //p.background(0);
-    p.fill(255);
-    p.noStroke();
+    curpos = [p.mouseX,p.mouseY]
 
-    //get prev pos
-    if (curpos == undefined) {
-        prevpos = [0,0]
-    } else {
-        prevpos = curpos
+    // DEBUG
+    
+    // curpos = [counter,counter/2]
+
+    dist = p.dist(curpos[0],curpos[1],plotpos[0],plotpos[1])
+    radius = 20
+    if (dist>radius&&dist>plotrad/2+radius) {
+        //style
+        counter ++
+        p.fill(p.color(`hsl(${(counter*182)%360}, 90%, 50%)`))
+        p.noStroke()
+        
+        //draw
+        let dia = dist-plotrad/2+radius
+        p.ellipse(curpos[0],curpos[1],dia,dia)
+        //p.ellipse(curpos[0],curpos[1],40,40)
+
+
+        console.log("dist = " + dist + " plotrad = " + plotrad)
+        plotpos=curpos
+        plotrad=dia
+        console.log(curpos[0])
+        
+        
+        p.stroke(0)
+        p.ellipse(curpos[0],curpos[1],1,1)
     }
-
-    //get cur pos
-    curpos = [p.mouseX,p.mouseY];
-
-    radius = p.dist(prevpos[0], prevpos[1], curpos[0], curpos[1])
-
-    if (p.mouseIsPressed) {
-        p.fill(0);
-      } else {
-        p.fill(255);
-      }
-    
-    p.ellipse(p.mouseX, p.mouseY, radius*2, radius*2);
-
-    
-
-    // counter ++
-    // p.fill(255);
-    // p.textAlign("CENTER");
-    // p.strokeWeight(4);
-    // p.stroke(0);
-    // p.text(fps,res[0]/2,res[1]/2)
-
-
-
-
     
   };
 };
