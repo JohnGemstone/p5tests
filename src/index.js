a2b0e2e86ad641a9f041994d5f85ef61fa593e51
Jim Johnston <hello@breadnbutterdesign.com>
2020-06-28 01:21:45 +0100

    vector fixed diameter

--- src/index.js
+++ src/index.js
@@ -4,52 +4,49 @@ const containerElement = document.getElementById('p5');
 
 
 const sketch = (p) => {
-  let res = [window.innerWidth,window.innerHeight]
-  let x = 100;
-  let y = 100;
   let counter = 0;
-  let curpos, plotpos, plotrad, radius, dist;
-  plotpos = [0,0]
-  plotrad = 0
-  
-  
+  let res, curpos, plotpos, plotrad, minrad, dist;
 
-  p.setup = function() {
-    p.createCanvas(res[0], res[1]);
-    //p.frameRate(10)
+  res = p.createVector(window.innerWidth,window.innerHeight)
+  curpos = p.createVector(0,0)
+  plotpos = p.createVector(0,0)
+  plotrad = 40
+  minrad = 40
+  
     console.log(p)
+  p.setup = function() {
+    p.createCanvas(res.x, res.y);
+
   };
 
   p.draw = function() {
-    curpos = [p.mouseX,p.mouseY]
 
-    // DEBUG
-    
-    // curpos = [counter,counter/2]
+   curpos.set(p.mouseX, p.mouseY)
+   
+   dist = curpos.dist(plotpos)
 
-    dist = p.dist(curpos[0],curpos[1],plotpos[0],plotpos[1])
-    radius = 20
-    if (dist>radius&&dist>plotrad/2+radius) {
-        //style
-        counter ++
-        p.fill(p.color(`hsl(${(counter*182)%360}, 90%, 50%)`))
-        p.noStroke()
-        
-        //draw
-        let dia = dist-plotrad/2+radius
-        p.ellipse(curpos[0],curpos[1],dia,dia)
-        //p.ellipse(curpos[0],curpos[1],40,40)
+   if (dist > plotrad/2+minrad) {
+
+    let dia = 40
+
+    //cal angle
+    p.angleMode("DEGREES")
+    let angle = p.atan2(plotpos.x-curpos.x,plotpos.y-curpos.y)*-1-(p.PI*0.5)
+    console.log((angle))
+
+    //draw circle
+    let newpos = new p5.Vector.fromAngle(angle,dia)
+    newpos.add(plotpos)
+    p.ellipse(newpos.x, newpos.y, dia)
+    p.ellipse(newpos.x, newpos.y, 1)
+
+    //set plot attributes
+    plotrad = dia
+    plotpos.set(newpos)
 
 
-        console.log("dist = " + dist + " plotrad = " + plotrad)
-        plotpos=curpos
-        plotrad=dia
-        console.log(curpos[0])
-        
-        
-        p.stroke(0)
-        p.ellipse(curpos[0],curpos[1],1,1)
-    }
+   }
+   
     
   };
 };
